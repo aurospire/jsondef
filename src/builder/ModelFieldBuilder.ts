07cba621b97e4db382ced5bfdb99f0204c3bd3a9
aurospire <aurospire@gmail.com>
2024-07-21 17:55:48 -0400

    Added Object and ModelBuilders

---
+++ src/builder/ModelFieldBuilder.ts
@@ -0,0 +1,33 @@
+import { FieldObject, ModelField } from "../Field";
+import { PositiveBoundedFieldBuilder } from "./PositiveBoundedFieldBuilder";
+
+
+export class ModelFieldBuilder<Of extends FieldObject, Optional extends boolean = false>
+    extends PositiveBoundedFieldBuilder<'model', Optional>
+    implements ModelField {
+
+    #of: Of;
+
+    #name: string;
+
+    constructor(from: ModelFieldBuilder<Of, Optional> | Of, name: string) {
+        super(from instanceof ModelFieldBuilder ? from : undefined);
+
+        this.#of = from instanceof ModelFieldBuilder ? from.#of : from;
+
+        this.#name = name;
+    }
+
+    get kind(): "model" { return 'model'; }
+
+    get of(): Of { return this.#of; }
+
+    get name(): string { return this.#name; }
+
+
+    override optional(): ModelFieldBuilder<Of, true> { return super.optional() as any; }
+
+    override required(): ModelFieldBuilder<Of, false> { return super.required() as any; }
+
+    protected override clone() { return new ModelFieldBuilder<Of, Optional>(this, this.name); }
+}
---
+++ src/builder/ObjectFieldBuilder.ts
@@ -0,0 +1,25 @@
+import { FieldObject, ObjectField } from "../Field";
+import { PositiveBoundedFieldBuilder } from "./PositiveBoundedFieldBuilder";
+
+export class ObjectFieldBuilder<Of extends FieldObject, Optional extends boolean = false>
+    extends PositiveBoundedFieldBuilder<'object', Optional>
+    implements ObjectField {
+
+    #of: Of;
+
+    constructor(from: ObjectFieldBuilder<Of, Optional> | Of) {
+        super(from instanceof ObjectFieldBuilder ? from : undefined);
+
+        this.#of = from instanceof ObjectFieldBuilder ? from.#of : from;
+    }
+
+    get kind(): "object" { return 'object'; }
+
+    get of(): Of { return this.#of; }
+
+    override optional(): ObjectFieldBuilder<Of, true> { return super.optional() as any; }
+
+    override required(): ObjectFieldBuilder<Of, false> { return super.required() as any; }
+
+    protected override clone() { return new ObjectFieldBuilder<Of, Optional>(this); }
+}
