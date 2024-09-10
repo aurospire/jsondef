55be6bf0a083fe5d99ece3e0c41c66a62f971c2f
aurospire <aurospire@gmail.com>
2024-09-10 12:16:14 -0400

    tsdoc for BaseSchemaBuilders

--- src/builder/AnySchemaBuilder.ts
+++ src/builder/AnySchemaBuilder.ts
@@ -1,19 +1,39 @@
 import { AnySchema } from "../Schema";
 import { BaseSchemaBuilder } from "./BaseSchemaBuilder";
 
+/**
+ * Builder class for creating AnySchemas.
+ * @template Optional - Whether the schema is optional or not.
+ */
 export class AnySchemaBuilder<const Optional extends boolean = false>
     extends BaseSchemaBuilder<'any', Optional>
     implements AnySchema {
 
+    /**
+     * Creates a new AnySchemaBuilder instance.
+     * @param from - Optional AnySchemaBuilder to copy attributes from.
+     */
     constructor(from?: AnySchemaBuilder<Optional>) { super(from); }
 
+    /** Gets the kind of schema, which is always 'any' for AnySchema */
     get kind(): "any" { return 'any'; }
 
+    /**
+     * Makes the schema optional.
+     * @returns A new AnySchemaBuilder instance with isOptional set to true.
+     */
+    override optional(): AnySchemaBuilder<true> { return super.optional() as AnySchemaBuilder<true>; }
 
-    override optional(): AnySchemaBuilder<true> { return super.optional() as any; }
+    /**
+     * Makes the schema required.
+     * @returns A new AnySchemaBuilder instance with isOptional set to false.
+     */
+    override required(): AnySchemaBuilder<false> { return super.required() as AnySchemaBuilder<false>; }
 
-    override required(): AnySchemaBuilder<false> { return super.required() as any; }
-
-
+    /**
+     * Creates a clone of the current builder.
+     * @returns A new AnySchemaBuilder instance with the same attributes.
+     */
     protected override clone() { return new AnySchemaBuilder<Optional>(this); }
 }
+
--- src/builder/RootSchemaBuilder.ts
+++ src/builder/RootSchemaBuilder.ts
@@ -1,18 +1,39 @@
 import { RootSchema } from "../Schema";
 import { BaseSchemaBuilder } from "./BaseSchemaBuilder";
 
+/**
+ * Builder class for creating RootSchemas.
+ * @template Optional - Whether the schema is optional or not.
+ */
 export class RootSchemaBuilder<const Optional extends boolean = false>
     extends BaseSchemaBuilder<'root', Optional>
     implements RootSchema {
 
+    /**
+     * Creates a new RootSchemaBuilder instance.
+     * @param from - Optional RootSchemaBuilder to copy attributes from.
+     */
     constructor(from?: RootSchemaBuilder<Optional>) { super(from); }
 
+    /** Gets the kind of schema, which is always 'root' for RootSchema */
     get kind(): "root" { return 'root'; }
 
-    override optional(): RootSchemaBuilder<true> { return super.optional() as any; }
+    /**
+     * Makes the schema optional.
+     * @returns A new RootSchemaBuilder instance with isOptional set to true.
+     */
+    override optional(): RootSchemaBuilder<true> { return super.optional() as RootSchemaBuilder<true>; }
 
-    override required(): RootSchemaBuilder<false> { return super.required() as any; }
+    /**
+     * Makes the schema required.
+     * @returns A new RootSchemaBuilder instance with isOptional set to false.
+     */
+    override required(): RootSchemaBuilder<false> { return super.required() as RootSchemaBuilder<false>; }
 
-
-    protected override clone() { return new RootSchemaBuilder(this); }
+    /**
+     * Creates a clone of the current builder.
+     * @returns A new RootSchemaBuilder instance with the same attributes.
+     */
+    protected override clone() { return new RootSchemaBuilder<Optional>(this); }
 }
+
