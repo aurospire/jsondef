1b92aad28c606dde0a3047197c2ca0f138d28a73
aurospire <aurospire@gmail.com>
2024-09-16 17:28:06 -0400

    Added tsdoc to stringify

--- src/stringify/stringify.ts
+++ src/stringify/stringify.ts
@@ -2,7 +2,14 @@ import { Schema } from "../Schema";
 import { StringifyFormat, condensedFormat, prettifyFormat } from "./StringifyFormat";
 import { stringifySchema } from "./stringifySchema";
 
-// Main stringify function
+/**
+ * Converts a Schema object to its string representation.
+ * 
+ * @param schema - The Schema object to stringify.
+ * @param format - Optional partial StringifyFormat object to customize the output format.
+ * @param condensed - Optional boolean flag to use condensed format (default is false).
+ * @returns A string representation of the input Schema.
+ */
 export const stringify = (schema: Schema, format: Partial<StringifyFormat> = {}, condensed: boolean = false): string => {
     const stringifyFormat = condensed ? condensedFormat(format) : prettifyFormat(format);
 
